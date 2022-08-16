f80dea5f4ab1feeac142501407f11e047d27bc5e
h-nriquevieira <h.nriquevieira@gmail.com>
2022-08-16 15:07:01 -0300

    Refactor CardProjeto component to use props

--- src/components/CardProjeto/CardProjeto.jsx
+++ src/components/CardProjeto/CardProjeto.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './CardProjeto.css'
 
-function CardProjeto() {
-  return (
+function CardProjeto(props) {
+    const tags = props.tags.map(tag => <p className='card--technology'>{tag}</p>)
+  
+    return (
       <div className="card">
-          <img className="card--img" src="/project-1.png" alt="" />
-          <h3 className="card--title">AutoOn</h3>
+          <img className="card--img" src={props.img} alt="" />
+          <h3 className="card--title">{props.title}</h3>
           <div className="card--tags">
-              <p className="card--technology">React</p>
-              <div className="card--project-type">Projeto de estudo</div>
+              {tags}
+              <div className="card--project-type">{props.type}</div>
           </div>
           <p className="card--links">
-              <a href="">Visitar Demo</a> | <a href="">GitHub</a>
+              <a href={props.url}>{props.demo ? "Visitar Demo" : "Visitar Página"}</a> | {props.github &&<a href="">GitHub</a>}
           </p>
           <p className="card--description">
-          Projeto fictício de concessionária de veículos feito em React. Cada veículo recebe um card gerado dinamicamente, mapeando os dados recebidos em JSON e gerando um novo componente para cada entrada. Inicialmente, foi utilizado um JSON fictício. O próximo passo é implementar um banco de dados real.
+          {props.description}
           </p>
       </div>
   );
--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import CardProjeto from '../CardProjeto/CardProjeto';
+import projects from '../../projects-data.json'
 import './Projetos.css'
 
 function Projetos() {
+  const projectCards = projects.map(project => (
+    <CardProjeto 
+      title={project.title}
+      img={project.img}
+      tags={project.tags}
+      type={project.type}
+      demo={project.demo}
+      url={project.url}
+      github={project.github}
+      description={project.description}
+    />
+  ))
+
   return (
     <section className="projetos">
       <div className="container">
         <h3 className="projetos--title">Projetos</h3>
-        <CardProjeto />
+        {projectCards}
       </div>
     </section>
   );
