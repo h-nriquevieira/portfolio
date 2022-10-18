ff110fd24c4c417b3998a3f75ec0a06ee951dc15
h-nriquevieira <h.nriquevieira@gmail.com>
2022-10-18 17:57:27 -0300

    Fix typo on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
               <p className='header--pre-title'>Olá, eu sou</p>
               <h1>Henrique Vieira</h1>
               <h2 className='header--subtitle'>Desenvolvedor Front-End | React</h2>
-              <a href="#projetos" className='header--btn'>Confira meu portifólio</a>
+              <a href="#projetos" className='header--btn'>Confira meu portfólio</a>
           </div>
       </header>
   );
